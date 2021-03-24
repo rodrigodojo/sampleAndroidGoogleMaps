01cb47ebe7ede67be40db2ea27c9197a733bf9bd
Rodrigo Dojo <rodrigodoj@gmail.com>
2021-03-24 12:27:45 -0300

    exemplo de Aplicação de Android com API Google Maps

---
+++ app/src/main/java/com/dojo/myapplication/MainActivity.java
@@ -0,0 +1,82 @@
+package com.dojo.myapplication;
+
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    static ArrayList<String> lugares;
+    static ArrayAdapter arrayAdapter;
+    String[] permissoes = new String[]{
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        PermissionsUtils.validate(this,0,permissoes);
+
+        ListView listView = findViewById(R.id.listView);
+        lugares = new ArrayList<>();
+        lugares.add("Selecione seu local favorito");
+        arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1,lugares);
+        listView.setAdapter(arrayAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(getApplicationContext(),MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        for (int result : grantResults) {
+            if (result == PackageManager.PERMISSION_DENIED) {
+                // Alguma permissão foi negada
+                alertAndFinish();
+                return;
+            }
+        }
+        // Se chegou aqui está OK
+    }
+
+    private void alertAndFinish() {
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.app_name).setMessage("Para utilizar este aplicativo, você precisa aceitar as permissões.");
+            // Add the buttons
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int id) {
+                    finish();
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/dojo/myapplication/PermissionsUtils.java
@@ -0,0 +1,39 @@
+package com.dojo.myapplication;
+
+import android.app.Activity;
+import android.content.pm.PackageManager;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermissionsUtils {
+    /**
+     * Solicita as permissões
+     */
+    public static boolean validate(Activity activity, int requestCode, String... permissions) {
+        List<String> list = new ArrayList<String>();
+        for (String permission : permissions) {
+            // Valida permissão
+            boolean ok = ContextCompat.checkSelfPermission(activity, permission) == PackageManager.PERMISSION_GRANTED;
+            if (! ok ) {
+                list.add(permission);
+            }
+        }
+        if (list.isEmpty()) {
+            // Tudo ok, retorna true
+            return true;
+        }
+
+        // Lista de permissões que falta acesso.
+        String[] newPermissions = new String[list.size()];
+        list.toArray(newPermissions);
+
+        // Solicita permissão
+        ActivityCompat.requestPermissions(activity, newPermissions, 1);
+
+        return false;
+    }
+}
